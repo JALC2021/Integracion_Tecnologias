0cad40e418b58d9587cb9ff021f682c3bce1cceb
jalc <jantonio2511@gmail.com>
2018-06-13 19:54:09 +0200

    eliminano y ceradno persitecia

--- pruebaJsp_junio_18/src/java/servlet/Controlador.java
+++ pruebaJsp_junio_18/src/java/servlet/Controlador.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author jalc
  */
-public class controlador extends HttpServlet {
+public class Controlador extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
